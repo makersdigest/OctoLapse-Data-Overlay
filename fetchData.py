58222cc569083c2e83feef5ba15f8aa791909ba5
kalebclark <makersdigest@gmail.com>
2019-01-03 16:57:12 +0000

    first commit

---
+++ fetchData.py
@@ -0,0 +1,71 @@
+#!/usr/bin/python
+import urllib2
+import json
+import sys
+
+output_file = "/opt/OctoPrintScripts/tlData/data.json"
+
+def getRequest(path):
+    octoprintUrl = "http://192.168.1.75:5000/api"
+    
+    fullPath = octoprintUrl + path
+
+    request = urllib2.Request(fullPath, headers = {
+        "X-Api-Key": "70D94A02DDD14699ADFB6820ADAEF17F"
+    })
+
+    data = urllib2.urlopen(request).read()
+
+    return json.loads(data)
+    
+
+def main():
+
+    # Process Job information
+    #########################
+    job = getRequest("/job")
+
+    # name, size
+    print_file = job['job']['file']
+
+    # completion, printTime, printTimeLeft
+    progress   = job['progress']
+
+    ## Process Printer Information
+    ##############################
+    printer = getRequest("/printer")
+
+    # actual, target
+    bed = printer['temperature']['bed']
+    
+    # actual, target
+    he  = printer['temperature']['tool0']
+
+    outputData = {
+        'print_file': { 'name': print_file['name'], 'size': print_file['size'] },
+        'progress': {
+            'progress': progress['completion'],
+            'print_time': progress['printTime'],
+            'print_time_left': progress['printTimeLeft']
+        },
+        'bed_temp': {'target': bed['target'], 'actual': bed['actual'] },
+        'he_temp': {'target': he['target'], 'actual': he['actual']}
+    }
+
+    ## Write file
+    #############
+    file = open(output_file, "w")
+    file.write(json.dumps(outputData))
+    file.close()
+
+
+
+
+
+
+
+
+main()
+
+
+
---
+++ overlay.py
@@ -0,0 +1,75 @@
+#!/usr/bin/python
+import json
+import subprocess
+import os
+import sys
+from scipy.interpolate import interp1d
+
+input_path = "/opt/OctoPrintScripts/tlData/data.json"
+input_path = "data.json"
+
+input_file = sys.argv[6]
+output_file = 'output.jpg'
+
+col_1 = 60
+col_2 = 123
+
+def main():
+    ## Open data file
+    #################
+    file = open(input_path, "r")
+    data = json.loads(file.read())
+
+    command = "convert " + input_file 
+
+    # Add Rectangle
+    command += " -fill '#0008' -draw 'rectangle 5,980,1910,1075'"
+
+    # Progress Bar - Outline
+    command += " -fill none -stroke white -strokewidth 1 -draw 'rectangle 10,985, 300, 1005'" 
+    
+    # progress bar fill
+    prog_range = [0, 100];
+    rect_range = [10, 300];
+    interp_prog = interp1d(prog_range, rect_range)
+    raw_progress = data['progress']['progress']
+    progress = interp_prog(raw_progress)
+
+    command += " -fill white -draw 'rectangle 10, 985, "+str(progress)+", 1005'"
+    command += " -fill white -pointsize 16 -annotate +305+1000 '"+str(round(raw_progress,1))+"%'"
+    # Add filename
+    #command += " -fill white -pointsize 16 -annotate +10+1000 'Filename: '"
+    #command += " -fill white -pointsize 16 -annotate +85+1000 '"+data['print_file']['name']+"'"
+
+    # Add Temperature Grid - Right, top, left, bottom
+    command += " -fill white -draw 'line 10, 1030, 200, 1030'"  # Horiz Line
+    command += " -fill white -draw 'line "+str(col_1)+", 1010, "+str(col_1)+", 1075'" # Vert 1
+    command += " -fill white -draw 'line "+str(col_2)+", 1010, "+str(col_2)+", 1075'" # vert 2
+    command += " -fill white -pointsize 14 -annotate +10+1023 'Temps'"
+    command += " -fill white -pointsize 14 -annotate +10+1047 'Target: '"
+    command += " -fill white -pointsize 14 -annotate +10+1067 'Actual: '"
+    command += " -fill white -pointsize 14 -annotate +"+str(col_1 + 5)+"+1023 'Hot End'"
+    command += " -fill white -pointsize 14 -annotate +"+str(col_2 + 5)+"+1023 'Bed'"
+
+    # Add Temperature Texts:
+    command += " -fill white -pointsize 14 -annotate +"+str(col_1 + 5)+"+1047 '"+str(data['he_temp']['target'])+"'"
+    command += " -fill white -pointsize 14 -annotate +"+str(col_1 + 5)+"+1067 '"+str(data['he_temp']['actual'])+"'"
+    command += " -fill white -pointsize 14 -annotate +"+str(col_2 + 5)+"+1047 '"+str(data['bed_temp']['target'])+"'"
+    command += " -fill white -pointsize 14 -annotate +"+str(col_2 + 5)+"+1067 '"+str(data['bed_temp']['actual'])+"'"
+
+
+
+    # Complete Commmand 
+    command += " "+output_file
+    os.system(command)
+
+    ## Overlay Text
+    #subprocess.call(['convert', 'test.jpg',
+    #    "-fill '#0008' -draw rectangle 5,980,1910,1075",
+    #    "-fill white -pointsize 10 -annotate +10+990 'foobar'",
+#
+#        'output.jpg'
+#    ])
+
+
+main()
